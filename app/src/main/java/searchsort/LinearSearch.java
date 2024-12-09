271ca5b278609a9e1514659062c0ff6213b82152
KarthikRaveeswaran <348899022@gapps.yrdsb.ca>
2024-12-09 12:54:37 -0500

    Update LinearSearch.java

--- app/src/main/java/searchsort/LinearSearch.java
+++ app/src/main/java/searchsort/LinearSearch.java
@@ -5,7 +5,12 @@ public class LinearSearch {
      * Find the index of the target element in the array arr using linear search.
      * Return -1 if target is not found in arr.
      */
-    public static int find(int[] arr, int target) {
-        return 0;
-    }
+   public static int find(int[] arr, int target) {
+       for (int i = 0; i < arr.length; i++) {
+           if (arr[i] == target) {
+               return i;
+           }
+       }
+       return -1;
+   }
 }
