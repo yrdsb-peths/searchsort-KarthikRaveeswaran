e2dad89f9fb1199c387c22b2d388551ab4af2210
KarthikRaveeswaran <348899022@gapps.yrdsb.ca>
2024-12-09 13:05:55 -0500

    Update SelectionSort.java

--- app/src/main/java/searchsort/SelectionSort.java
+++ app/src/main/java/searchsort/SelectionSort.java
@@ -1,10 +1,29 @@
 package searchsort;
 
 public class SelectionSort {
-    /**
-     * Sort the array arr using selection sort algorithm.
-     */
-    public static void sort(int[] arr) {
+   /**
+    * Sort the array arr using selection sort algorithm.
+    */
+   public static void sort(int[] arr) {
+       int n = arr.length;
 
-    }
+
+       // Traverse the array
+       for (int i = 0; i < n - 1; i++) {
+           // Find the minimum element in unsorted part of the array
+           int minIndex = i;
+           for (int j = i + 1; j < n; j++) {
+               if (arr[j] < arr[minIndex]) {
+                   minIndex = j; // Update the index of the minimum element
+               }
+           }
+
+
+           // Swap the found minimum element with the element at index i
+           int temp = arr[i];
+           arr[i] = arr[minIndex];
+           arr[minIndex] = temp;
+       }
+   }
 }
+
